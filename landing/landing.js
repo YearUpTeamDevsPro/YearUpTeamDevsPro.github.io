b23c4512ca2f3199537e8836854023edaed217a0
AlmateenAllanah <almateenallanah18@gmail.com>
2023-01-05 09:48:19 -0500

    edited script

--- landing/landing.js
+++ landing/landing.js
@@ -2,26 +2,26 @@
 
 "use strict";
 
-const loginForm = document.getElementById("#login");
-const loginButton = document. document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const loginForm = document.getElementById("#loginForm");
 
-loginButton.addEventListener("click", (e)) => {
+loginForm.onsubmit = function (e) {
     e.preventDefault()
     // Prevent the form from refreshing the page,
     // as it will do by default when the Submit event is triggered
 
     // We can use loginForm.username (for example) to access
     // the input element in the form which has the ID of "username".
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if(username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
+    const loginData = {
+        username: loginForm.username.value,
+        password: loginForm.password.value,
     }
+
+    // if(username === "user" && password === "web_dev") {
+    //     alert("You have successfully logged in.");
+    //     location.reload();
+    // } else {
+    //     loginErrorMsg.style.opacity = 1;
+    // }
 
     // Disables the button after the form has been submitted already:
     loginForm.loginButton.disabled = true;
