fb11b219b65f3874739dde195a9ab10e580df7b5
Safat Mobid <safatmobid@gmail.com>
2023-01-06 12:08:01 -0500

    finished troubleshoot

--- profile/profile.js
+++ profile/profile.js
@@ -53,49 +53,49 @@ window.onload = () =>{
         }
     
 
+        addPost.onsubmit = (submit) =>{
 
-
-}
-
-
-addPost.onsubmit = (submit) =>{
-
-    let textArea1 = document.getElementById("textArea1").value;
-    console.log(JSON.stringify({ text: textArea1}));
-        fetch('https://microbloglite.herokuapp.com/api/posts', {
-            method: 'POST',
-            headers: {
-                'Accept' : 'application/json, text/plain, */*',
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${(getLoginData()).token}`
-               
-             },
-                    body: JSON.stringify({ text: textArea1})
+            let textArea1 = document.getElementById("textArea1").value;
+            console.log(JSON.stringify({ text: textArea1}));
+                fetch('https://microbloglite.herokuapp.com/api/posts', {
+                    method: 'POST',
+                    headers: {
+                        'Accept' : 'application/json, text/plain, */*',
+                        'content-type': 'application/json',
+                        'Authorization': `Bearer ${(getLoginData()).token}`
+                       
+                     },
+                            body: JSON.stringify({ text: textArea1})
+                  
+        
+        
+                })
+                .then((response) => response.json())
+                .then((body) => {
+                    let PostOutput = `<h3>Post</h3>`;
+                    PostOutput += ` <ul>
+                   
+                    <li> ID: ${body._id}</li>
+                    <li> Created At: ${body.createdAt.toLocaleString()}</li>
+                    <li> Text: ${body.text}</li>
+                    <li> User Name: ${body.username}</li>
+                    
+                    </ul>`;
+        
+        
+                     document.getElementById("PostOutput").innerHTML = PostOutput; 
+                     console.log(body);
+                })
+        
           
-
-
-        })
-        .then((response) => response.json())
-        .then((body) => {
-            let PostOutput = `<h3>Post</h3>`;
-            PostOutput += ` <ul>
-           
-            <li> ID: ${body._id}</li>
-            <li> Created At: ${body.createdAt.toLocaleString()}</li>
-            <li> Text: ${body.text}</li>
-            <li> User Name: ${body.username}</li>
-            
-            </ul>`;
-
-
-             document.getElementById("PostOutput").innerHTML = PostOutput; 
-             console.log(body);
-        })
-
-  
-
+        
+        
+        }
 
 }
+
+
+
 function userProfileName(){
     let userName = document.getElementById("userName");
     // userName.innerHTML = username;
